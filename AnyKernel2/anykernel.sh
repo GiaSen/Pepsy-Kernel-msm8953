96796b409539d933fed8834db88b7bf274a54096
GiaSen <giammasena@gmail.com>
2019-01-30 15:36:43 +0100

    Add AnyKernel2, build script and wercker config

---
+++ AnyKernel2/anykernel.sh
@@ -0,0 +1,44 @@
+# AnyKernel2 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() {
+kernel.string=Pepsy Kernel by GiaSen @ xda-developers
+do.devicecheck=1
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=1
+device.name1=mido
+device.name2=Redmi Note 4
+device.name3=Redmi Note 4x
+device.name4=HMNote4x
+device.name5=Redmi Note 4X
+} # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+## end setup
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. /tmp/anykernel/tools/ak2-core.sh;
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+chmod -R 750 $ramdisk/*;
+chown -R root:root $ramdisk/*;
+
+
+## AnyKernel install
+dump_boot;
+
+
+
+write_boot;
+
+## end install
---
+++ wercker.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+dpkg --add-architecture i386 && apt-get update && apt-get install -y git ccache automake bc lzop bison gperf build-essential zip curl zlib1g-dev zlib1g-dev:i386 g++-multilib python-networkx libxml2-utils bzip2 libbz2-dev libbz2-1.0 libghc-bzlib-dev squashfs-tools pngcrush schedtool dpkg-dev liblz4-tool make optipng &&
+git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 /pipeline/build/root/toolchain/aarch64-linux-android-4.9
+
+KERNEL_DIR=$PWD
+ANYKERNEL_DIR=$KERNEL_DIR/AnyKernel2
+TOOLCHAINDIR=/pipeline/build/root/toolchain/aarch64-linux-android-4.9
+CCACHEDIR=../CCACHE/mido
+DATE=$(date +"%d%m%Y")
+KERNEL_NAME="Pepsy-Kernel"
+DEVICE="-mido-"
+VER="-v0.2"
+TYPE="-P"
+FINAL_ZIP="$KERNEL_NAME""$DEVICE""$DATE""$TYPE""$VER".zip
+
+rm $ANYKERNEL_DIR/Image.gz-dtb
+rm $KERNEL_DIR/out/arch/arm64/boot/Image.gz $KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
+
+export ARCH=arm64
+export KBUILD_BUILD_USER="GiaSen"
+export KBUILD_BUILD_HOST="Pepsy"
+export CROSS_COMPILE=/pipeline/build/root/toolchain/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export LD_LIBRARY_PATH=/pipeline/build/root/toolchain/aarch64-linux-android-4.9/lib/
+export USE_CCACHE=1
+export CCACHE_DIR=$CCACHEDIR/.ccache
+make clean O=out/
+make mrproper O=out/
+make mido_defconfig O=out/
+curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d text="$KERNEL_NAME$VER build started!" -d chat_id=@pepsy_mido;
+make -j$( nproc --all ) O=out/
+
+{
+
+  #try block
+
+cp $KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb $ANYKERNEL_DIR/
+
+} || {
+
+  #catch block
+
+  if [ $? != 0 ]; then
+
+    curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d text="Build failed :c" -d chat_id=@pepsy_mido;
+    exit
+
+  fi
+
+}
+cd $ANYKERNEL_DIR/
+zip -r9 $FINAL_ZIP * -x *.zip $FINAL_ZIP
+message="Build completed with the latest commit -"
+curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d text="$message $(git log --pretty=format:'%h : %s' -1)" -d chat_id=@pepsy_mido
+curl -F chat_id="-1001138151459" -F document=@"$FINAL_ZIP" https://api.telegram.org/bot$BOT_API_KEY/sendDocument
+mv $FINAL_ZIP /pipeline/output/$FINAL_ZIP
